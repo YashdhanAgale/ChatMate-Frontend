a3b9a7434478db35cd9a11957a9ce5026bb1c35e
yashodhanAgale <yashodhanagale3@gmail.com>
2025-08-09 01:04:39 +0530

    deployment fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://chatmate-backend-lvdl.onrender.com/api",
 });
 
 export const getConversations = () => API.get("/conversations");
